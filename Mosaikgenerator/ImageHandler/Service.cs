8a0f4a7b675dd10279e5ba7f30119b11e6866907
Salvos <tim.gabriel@t-online.de>
2017-02-26 13:33:24 +0100

    Imagehandler eingefuegt

---
+++ Mosaikgenerator/ImageHandler/Service.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ImageHandler
+{
+    class Service
+    {
+        static void Main(string[] args)
+        {
+            ServiceHost host = null;
+
+            try
+            {
+
+                host = new ServiceHost(typeof(Handler));
+                host.AddServiceEndpoint(typeof(Handler), new BasicHttpBinding(), new Uri("http://localhost:8080/mosaikgenerator/imagehandler"));
+                host.Open();
+
+            }
+            catch (Exception)
+            {
+
+                host.Close();
+
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("Enter to Exit");
+            Console.ReadLine();
+        }
+    }
+}
